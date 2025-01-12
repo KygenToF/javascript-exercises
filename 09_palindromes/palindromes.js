fb2f216f1034cce4cfacc375172e4a2c42e39e24
KygenToF <154561720+KygenToF@users.noreply.github.com>
2025-01-11 23:12:44 -0800

    complete all javascript exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = string.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const reversedString = [...newString].reverse().join('');
+    return newString === reversedString
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const yearsAlive = array.map(object => {
+        object.yearOfDeath ??= new Date().getFullYear();
+        return object.yearsAlive = object.yearOfDeath - object.yearOfBirth;
+    });
+    return array[yearsAlive.indexOf(Math.max(...yearsAlive))];
 };
 
 // Do not edit below this line
